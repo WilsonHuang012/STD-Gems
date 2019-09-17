6daaf250069df8a17196ce0a31e7808dffa527fb
WilsonHuang <wilson.huang.012@gmail.com>
2019-09-17 18:40:36 +0800

    std::copy Normal Copy

--- StdGems/Main.cpp
+++ StdGems/Main.cpp
@@ -1,4 +1,36 @@
+#include <vector>
+#include <string>
+#include <iterator>
+#include <iostream>
+#include <conio.h>
+
+template<class Container>
+void write_to_cout(const Container& container, const char* delimiter = " ")
+{
+	std::copy(container.begin(), container.end(),
+		std::ostream_iterator<Container::value_type>(std::cout, delimiter));
+}
+
+void Copy()
+{
+	std::vector<std::string> a =
+	{ "zero", "one", "two", "three", "four",
+	  "five", "six", "seven", "eight", "nine", "ten"
+	};
+	std::vector<std::string> b =
+	{ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+
+	write_to_cout(a);
+	std::cout << std::endl;
+	write_to_cout(b);
+	std::cout << std::endl << std::endl;
+
+	std::copy(a.begin() + 3, a.begin() + 7, b.begin() + 6);
+	write_to_cout(b);
+}
+
 int main()
 {
-	return 0;
+	Copy();
+	while (!_kbhit());
 }
