17a84cfde6d8d88f21b827a42f54cb1ecb7aecac
WilsonHuang <wilson.huang.012@gmail.com>
2019-09-30 10:18:52 +0800

    remove_if & unique

--- StdGems/Main.cpp
+++ StdGems/Main.cpp
@@ -13,7 +13,39 @@ void write_to_cout(const Container& container, const char* delimiter = " ")
 		std::ostream_iterator<Container::value_type>(std::cout, delimiter));
 }
 
-void Copy()
+void Copy(std::vector<std::string> a, std::vector<std::string> b)
+{
+	std::cout << "Copy if:" << std::endl;
+	std::copy_if(std::make_move_iterator(a.begin()),
+		std::make_move_iterator(a.end()), b.begin(),
+		[](const std::string& str) { return str.size() > 3; });
+	write_to_cout(a, " | ");
+	std::cout << std::endl;
+	write_to_cout(b);
+}
+
+template<class Container, class Pred>
+void Remove_If(Container a, Pred pred)
+{
+	std::cout << "Remove_If:" << std::endl;
+	const auto new_end = std::remove_if(a.begin(), a.end(), pred);
+	a.erase(new_end, a.end());
+	write_to_cout(a, " | ");
+}
+
+void Unique()
+{
+	std::cout << "Unique " <<
+		"(Removes all but the first element from every consecutive group " << 
+		"of equivalent elements in the range ):" << std::endl;
+	std::vector<std::string> b =
+	{ "0", "2", "2", "2", "4", "9", "6", "7", "9", "9", "10" };
+	const auto new_end = std::unique(b.begin(), b.end());
+	b.erase(new_end, b.end());
+	write_to_cout(b);
+}
+
+int main()
 {
 	std::vector<std::string> a =
 	{ "zero", "one", "two", "three", "four",
@@ -27,16 +59,17 @@ void Copy()
 	write_to_cout(b);
 	std::cout << std::endl << std::endl;
 
-	std::copy_if(std::make_move_iterator(a.begin()),
-		std::make_move_iterator(a.end()), b.begin(),
-		[](const std::string& str) { return str.size() > 3; });
-	write_to_cout(a, " | ");
-	std::cout << std::endl;
-	write_to_cout(b);
-}
+	Copy(a, b);
+	std::cout << std::endl << std::endl;
+	
+	auto pred = [](const std::string& s)
+	{
+		return std::count(s.begin(), s.end(), 'e') == 0;
+	};
+	
+	Remove_If(a, pred);
+	std::cout << std::endl << std::endl;
+	Unique();
 
-int main()
-{
-	Copy();
 	while (!_kbhit());
 }
