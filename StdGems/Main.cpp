93cdaf928388e62418a5597d50d0c9fcb00a3159
Wilson Huang <window012@hotmail.com>
2020-07-25 12:58:42 +0800

    map

--- StdGems/Main.cpp
+++ StdGems/Main.cpp
@@ -1,31 +1,53 @@
 #include <iostream>
-#include <sstream>
-#include <optional>
-
-std::optional<int> Search(int target)
-{
-    int array[5] = { 4, 1, 3, 2, 5 };
-    for (int i = 0; i < 5; i++)
-    {
-        if (array[i] == target)
-        {
-            return array[i];
-        }
-    }
-    return {};
-}
+#include <string>
+#include <map>
+#include <algorithm>
 
 int main()
 {
-    std::optional<int> x;
-    x = Search(7);
-    if (x)
+    std::map<std::string, int> map;
+    map.emplace("d", 4);
+    map.emplace("e", 5);
+    map.emplace_hint(map.find("d"), "h", 6);
+    map.insert({ "a", 1 });
+    map.insert({ "c", 3 });
+    map.insert({ "b", 2 });
+    //access an element doesn't exist
+    map["i"]; //insert the element with default value
+
+    for (auto m : map)
     {
-        std::cout << *x;
+        std::cout << m.first << " : " << m.second << std::endl;
     }
-    else
+    
+    std::cout << std::endl;
+    std::cout << "insert a existed key : a" << std::endl;
+    auto target = map.emplace("a", 0);
+    if (!target.second) //insert failed
     {
-        std::cout << "Not Found";
+        std::cout << "key \"a\" is existed { "
+            << target.first->first << " : " << target.first->second
+            << " }"
+            << std::endl;
     }
+    
+    std::cout << std::endl << "Remove value greater than 2" << std::endl;
+    for (auto i = map.begin(); i != map.end(); )
+    {
+        if (i->second > 2)
+        {
+            i = map.erase(i);
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    for (auto m : map)
+    {
+        std::cout << m.first << " : " << m.second << std::endl;
+    }
+
     return 0;
 }
